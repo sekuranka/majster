282935747f75c5e6a34a5d66909b8c2f84f95dba
sekuranka <anngab@st.amu.edu.pl>
2014-11-03 22:44:09 +0100

    no dodałam okienka Dawida, ania

---
+++ unity/Assets/textScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class textScript : MonoBehaviour {
+	public InputField userInput;
+	public Text roboTalk;
+	private string commandText = "";
+	void Awake () { 
+		userInput = GameObject.Find ("userInput").GetComponent<InputField>();
+		userInput.onSubmit.AddListener (((value) => OnSubmit(value)));
+		//userInput.validation = InputField.Validation.Alphanumeric;	
+	}
+	void OnSubmit(string line) {
+		Debug.Log ("OnSubmit("+line+")");
+		commandText += DateTime.Now.ToString("h:mm:ss tt") +  "\n" + line + "\n" ;
+	}
+	void OnGUI () {
+		GUI.TextArea (new Rect(668 ,125, 130,300),commandText,250);	
+	}
+}
