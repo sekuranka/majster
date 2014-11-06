33aec299b49fa270e1abe44675aeeb13683db573
siulkilulki <jurkiewiczdawid@gmail.com>
2014-11-06 16:19:23 +0100

    Update
    przygotowanie miejsca pod parser zosi

--- unity/Assets/textScript.cs
+++ unity/Assets/textScript.cs
@@ -6,17 +6,26 @@ using System;
 public class textScript : MonoBehaviour {
 	public InputField userInput;
 	public Text roboTalk;
-	private string commandText = "";
+	private string commandText, output, roboText = "";
 	void Awake () { 
 		userInput = GameObject.Find ("userInput").GetComponent<InputField>();
 		userInput.onSubmit.AddListener (((value) => OnSubmit(value)));
 		//userInput.validation = InputField.Validation.Alphanumeric;	
 	}
-	void OnSubmit(string line) {
-		Debug.Log ("OnSubmit("+line+")");
-		commandText += DateTime.Now.ToString("h:mm:ss tt") +  "\n" + line + "\n" ;
+
+	string Parser(string command) {
+		/* miesjce na parser zosi */
+		return "to ja robot i mowie";
 	}
+
+	void OnSubmit(string line) {	
+		Debug.Log ("OnSubmit("+line+")");
+		commandText = DateTime.Now.ToString("h:mm:ss tt") +  "\nUzytkownik: \n   "+ line + "\n" ;
+		roboText = Parser (line);
+		output += commandText + "Robot:\n" + "   "+roboText;
+	}
+
 	void OnGUI () {
-		GUI.TextArea (new Rect(668 ,125, 130,300),commandText,250);	
+		GUI.TextArea (new Rect(668 ,125, 130,300),output + "",250);	
 	}
 }
